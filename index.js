bf7ed7c92994360b09fe7c6ba1b4723318994c65
mariaclara31 <claramonteiro.21@gmail.com>
2020-10-03 11:15:21 -0300

    Teste NodeMailer

---
+++ index.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: "br758.hostgator.com.br",
+    port: 25,
+    secure: false,
+    auth: {
+        user: "noreply@devlanguage.com.br",
+        pass: "testpassword"
+    },
+    tls: { rejectUnauthorized: false }
+});
+
+const mailOptions = {
+    from: 'noreply@devlanguage.com.br',
+    to: 'clara@devlanguage.com.br',
+    subject: 'Sending Email using node.js',
+    text: 'Simple test!'
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
